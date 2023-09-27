4041ceef031b3dab75bc0f5e92bdc6f9b1a5fc69
frankiejun <frankiejun@gmail.com>
2023-09-27 22:49:57 +0800

    设定默认值

--- oralive.py
+++ oralive.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import time
-import psutil
 import argparse
 import sched
 import sys
 import ctypes
 import threading
 import os
-import cgroups
 
 parser = argparse.ArgumentParser(description='oracle服务器保活程序')
 
@@ -19,10 +17,13 @@ parser.add_argument('-m' '--memory', dest='mem', help='占用内存(MB)')
 parser.add_argument('-n', '--netinterval', dest='netinterval', help='网络占用的时间间隔(分钟)')
 args = parser.parse_args()
 
+
 if args.pertime is not None:
     time_limit = float(args.pertime)
     if time_limit <= 0:
         time_limit = 60
+else:
+    time_limit = 60
 
 sc = sched.scheduler(time.time, time.sleep)
 netsc = sched.scheduler(time.time, time.sleep)
@@ -31,22 +32,30 @@ if args.netinterval is not None:
     netinterval = int(args.netinterval)
     if netinterval <= 1:
         netinterval = 30
+else:
+    netinterval = 30
 
 if args.mem is not None:
     mem = int(args.mem)
     if mem <= 0 :
         mem = 100
+else:
+    mem = 0
 
 if args.occu_rate is not None:
     occu_rate = int(args.occu_rate)
     if occu_rate <= 1:
         occu_rate = 20
+else:
+    occu_rate = 20
 
 if args.interval is not None:
     ginterval = int(args.interval)
     if ginterval <= 0 :
         ginterval = 3
-    
+else:
+    ginterval = 1
+
 def GetTickCount():
     now = time.monotonic()
     return int(now * 1000)
@@ -55,7 +64,7 @@ def GetTickCount():
 def cpuCost():
     start_time = time.time()
     interval = 100
-    busyTime = interval * occu_rate / 100
+    busyTime = int(interval * occu_rate / 100)
     idleTime = interval - busyTime
     startTime = 0
 
@@ -97,8 +106,4 @@ def netWaste():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-      parser.print_help()
-      exit()
-    
     main()
